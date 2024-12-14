10944193a544df4e372d860f7531b4a0d81dae5d
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-12-14 15:19:08 +0000

    Create ex-2.js

---
+++ Array/ex-2.js
@@ -0,0 +1,8 @@
+//Write a JavaScript function to clone an array.
+
+var array_Clone = function(arra1) {
+  return arra1.slice(0);
+};
+
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
