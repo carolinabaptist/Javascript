443d30a0f5d196913660460c90694bb6dbbacd50
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-12-14 15:21:59 +0000

    Create ex-3.js

---
+++ Array/ex-3.js
@@ -0,0 +1,11 @@
+//Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
+
+var first = function(array, n) {
+  if (array == null)
+    return void 0;
+  if (n == null)
+    return array[0];
+  if (n < 0)
+    return [];
+  return array.slice(0, n);
+};
