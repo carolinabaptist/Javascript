61df254869abf6b47225c3f23648b7ac075608d1
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-12-14 14:49:11 +0000

    Create ex-2.js

---
+++ Basic/ex-2.js
@@ -0,0 +1,6 @@
+//Write a JavaScript program to print the current window contents.  
+
+function print_current_page() {
+    // Call the window.print() method to initiate the printing of the current page
+    window.print();
+} 
