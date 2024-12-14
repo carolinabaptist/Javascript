b18477612f218db6d5683dba01636ee521be9ffa
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-12-14 17:20:00 +0000

    Create ex-1.js

---
+++ Drawing/ex-1.js
@@ -0,0 +1,12 @@
+//Write a JavaScript program to draw the following rectangular shape.
+
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+
+    context.fillRect(20,20,100,100);
+    context.clearRect(40,40,60,60);
+    context.strokeRect(45,45,50,50);
+  }
+}
